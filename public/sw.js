ded4e174888769a62377b35bc4e739a29ee67a49
Estelle OLAWINSKI <estelle@fr.mpitech.com>
2020-04-18 17:59:45 +0200

    cache offline

--- public/sw.js
+++ public/sw.js
@@ -17,3 +17,15 @@ self.addEventListener("install", function (event) {
 self.addEventListener("activate", function () {
   console.log("SW activated");
 });
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (res) {
+      if (res) {
+        return res;
+      } else {
+        return fetch(event.request);
+      }
+    })
+  );
+});
